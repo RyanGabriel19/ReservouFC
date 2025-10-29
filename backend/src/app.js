bf626470df33a63910f3ea5b44de6d143422e9a9
Ryan <ryangabriel.apf19@gmail.com>
2025-10-29 10:03:37 -0300

    Adicionando arquivo vercel.json para consertar 404 - not found

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import usuarioRoutes from './routes/UsuarioRoute.js'
-// import  from './routes/ReservaRoute'
-// import  from './routes/QuadraRoute'
-// import  from './routes/LogRoute'
-// import  from './routes/HorarioBloqueadoRoute'
-// import  from './routes/DisponibilidadeRoute'
-// import  from './routes/DiaFechadoRoute'
 
 const app = express()
 const PORT = process.env.PORT || 3000;
