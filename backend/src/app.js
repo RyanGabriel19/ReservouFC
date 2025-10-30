a67478f847f87ef2fc28343cd10cbb227fd51d34
Ryan <ryangabriel.apf19@gmail.com>
2025-10-30 17:13:57 -0300

    Tentando ajustar bug cors no backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,11 +3,22 @@ import cors from 'cors';
 import usuarioRoutes from './routes/UsuarioRoute.js'
 
 const app = express()
+
+const allowedOrigin = 'https://reservou-fc.vercel.app';
+
+const corsOptions = {
+  origin: allowedOrigin
+};
+
+app.use(cors(corsOptions));
+
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 app.use(usuarioRoutes)
+
+
 
 app.listen(PORT, '0.0.0.0', ()=>{
     console.log(`servidor rodando na porta ${PORT}`)
