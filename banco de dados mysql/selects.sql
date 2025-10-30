8af4054e9dead33155e5d2444aedfac4f8bdca9f
Ryan <ryangabriel.apf19@gmail.com>
2025-10-30 15:24:16 -0300

    adicionando coluna TIPO na tabela QUADRA

--- banco de dados mysql/selects.sql
+++ banco de dados mysql/selects.sql
@@ -2,5 +2,10 @@
 SET time_zone = '-03:00'; -- Ajustar horário para o log
 select * from USUARIO;
 select * from LOG;
+select * from QUADRA;
+
+ALTER TABLE QUADRA DROP COLUMN TIPO;
+
+ALTER TABLE QUADRA ADD COLUMN tipo VARCHAR(255) NOT NULL;
 
 update USUARIO set tipo = 'a' where id = 17;
