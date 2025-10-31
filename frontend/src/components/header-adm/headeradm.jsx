93d659e126ee012d0212278fec2dbdfd4f55c04f
Mateus Nascimento <mateusnascimenthrp@gmail.com>
2025-10-31 13:08:31 -0300

    feito

--- frontend/src/components/header-adm/headeradm.jsx
+++ frontend/src/components/header-adm/headeradm.jsx
@@ -1,13 +1,13 @@
-import styles from './Headeradm.module.css';
+import styles from './headeradm.module.css';
 import { NavLink } from 'react-router-dom';
 
 function Headeradm() {
   return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
+    <header className={styles["header"]}>
+      <div className={styles["logo"]}>
         <h1 className={['h1headeradm']}>ReservouFC <span>- Admin</span></h1>
       </div>
-      <nav className={styles.navegacao}>
+      <nav className={styles["navegacao"]}>
         <NavLink to="/home" className={({ isActive }) => isActive ? styles.active : ''}>Home-ADM</NavLink>
         <NavLink to="/conta/admin" className={({ isActive }) => isActive ? styles.active : ''}>Painel de Controle</NavLink>
         <NavLink to="/agendamentoadm" className={({ isActive }) => isActive ? styles.active : ''}>Agendamento-ADM</NavLink>
